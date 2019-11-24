f907e41a6a215846a1dbfaa2264ae8b535f925bd
Stas <l.dior@ukr.net>
2019-11-25 00:18:31 +0200

    les 7

--- src/App.js
+++ src/App.js
@@ -2,25 +2,39 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => {
+    return (
+        <div>
+            <Header/>
+            <Technologies/>
+        </div>
+    );
+};
+
+const Header = () => {
+    return (
+        <div>
+            <h1>LOGO</h1>
+            <a href="#"> Главная</a>
+            <a href="#"> Услуги</a>
+            <a href="#"> Контакты</a>
+        </div>
+    );
+};
+
+const Technologies = () => {
+    return (
+        <div>
+            <ul>
+                <li>HTML</li>
+                <li>CSS</li>
+                <li>JS</li>
+                <li>REACT</li>
+            </ul>
+        </div>
+    );
+};
+
 
 export default App;
